3962f32b28e3af71333265bf28769d7582a465e0
Mark Stacy <mbstacy@gmail.com>
2017-02-07 15:34:34 -0600

    added datacatalog option

--- imageq/tasks/tasks.py
+++ imageq/tasks/tasks.py
@@ -69,7 +69,7 @@ def processimage(inpath, outpath, outformat="TIFF", filter="ANTIALIAS", scale=No
     return "{0}/oulib_tasks/{1}".format(hostname, task_id)
 
 @task()
-def catalog_derivative_gen(bags,outformat="TIFF", filter="ANTIALIAS", scale=None, crop=None):
+def catalog_derivative_gen(bags,outformat="TIFF", filter="ANTIALIAS", scale=None, crop=None, datacatalog=True):
     """
     Digilab data catalog derivative generation
 
@@ -87,15 +87,22 @@ def catalog_derivative_gen(bags,outformat="TIFF", filter="ANTIALIAS", scale=None
     os.makedirs(resultpath)
     #s3 boto
     s3 = boto3.resource('s3')
+    #get data catalog token
+    f1=open('~/.aws/cybercom_token','r')
+    token = "Token {0}".format(f1.read())
+    f1.close()
+    headers ={'Content-Type':'application/json',"Authorization":token}
+    url_new_catalog="https://cc.lib.ou.edu/api/catalog/data/catalog/digital_objects/.json"
     #select each bag
     for bag in bags.split(','):
-        url_template="%s/api/catalog/data/catalog/bagit_inventory/.json?page_size=1&query={'filter':{'s3.valid':True,'bag':'%s'},'projection':{'s3':1}}"
+        url_template="%s/api/catalog/data/catalog/bagit_inventory/.json?page_size=1&query={'filter':{'s3.valid':True,'bag':'%s'}}"
         r=requests.get(url_template % (hostname,bag))
         data=r.json()["results"]
         src_input = os.path.join(resultpath,'src/',bag)
         output = os.path.join(resultpath,'derivative/',bag)
         os.makedirs(src_input)
         os.makedirs(output)
+        derivatives={}
         #download source files
         for itm in data:
             bucket = itm['s3']['bucket']
@@ -104,8 +111,25 @@ def catalog_derivative_gen(bags,outformat="TIFF", filter="ANTIALIAS", scale=None
                     inpath="{0}/{1}".format(src_input,fle.split('/')[-1])
                     s3.meta.client.download_file(bucket, fle, inpath)
                     outpath="{0}/{1}.{2}".format(output,fle.split('/')[-1].split('.')[0].lower(),outformat)
+                    out_url = "{0}/oulib_tasks/{1}/derivative/{2}.{3}".format(hostname, task_id,fle.split('/')[-1].split('.')[0].lower(),outformat)
                     _processimage(inpath=inpath,outpath=outpath,outformat=outformat,filter=filter,scale=scale,crop=crop)
+                    derivatives[fle.split('/')[-1].split('.')[0].lower()]={"location":out_url,"outformat":outformat,"filter":filter,"scale":scale,"crop":crop}
                     os.remove(inpath)
         shutil.rmtree(os.path.join(resultpath,'src/',bag))
+        #set data catalog new inventory
+        if datacatalog:
+            req = requests.get(url_new_catalog + "?query= {'filter':{'bag':'%s'}}" % (bag))
+            d1=req.json()
+            if d1['count']>0:
+                newdata=d1['results'][0]
+                newdata['derivatives']=derivatives
+            else:
+                newdata={}
+                newdata['locations']={'s3':data[0]['s3'],'nas':data[0]['nas'],'norfile':data[0]['norfile']}
+                newdata['bag']=bag
+                newdata['department']=data[0]['department']
+                newdata['project']=data[0]['project']
+                newdata['derivatives']=derivatives
+            req=requests.post(url_new_catalog,data=json.dumps(newdata),headers=headers)         
     shutil.rmtree(os.path.join(resultpath,'src/'))
     return "{0}/oulib_tasks/{1}".format(hostname, task_id)
