fba99bb94e4a7726a0c186124648c2909ecefa72
Tyler Pearson <tdpearson@users.noreply.github.com>
2023-03-03 11:00:49 -0600

    tick version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='imageq',
-      version='0.4',
+      version='0.4.1',
       packages= find_packages(),
       install_requires=[
           'celery==5.2.7',
